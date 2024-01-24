742f6f1f7f7641384ac278cd3f4f3e4076a0719f
Alvaro Guimarães <62889807+alvarogfn@users.noreply.github.com>
2024-01-24 01:34:20 -0300

    Adiciona componentes de icones, configura o prettierc e sobe .vscode (#27)
    * chore: bump dependabot
    
    * feat: add new icons
    
    * chore: add .vscode in repository
    
    * chore: update prettier config to sort tailwind properties
    
    * style: just prettier

---
+++ components/icons/menu.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    width="24"
+    height="25"
+    viewBox="0 0 24 25"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M3 18.5V16.5H21V18.5H3ZM3 13.5V11.5H21V13.5H3ZM3 8.5V6.5H21V8.5H3Z"
+      fill="#F2F2F2"
+    />
+  </svg>
+</template>
